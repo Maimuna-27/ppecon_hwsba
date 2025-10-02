8926f495a4bf5c1f4435628eeca5be1ba5fd0de5
MiM <mohamed.ismail@ptco.com.sa>
2025-10-02 19:23:38 +0300

    payment entry

--- ppecon_hwsba/override/payment_entry.py
+++ ppecon_hwsba/override/payment_entry.py
@@ -197,13 +197,11 @@ class CustomPaymentEntry(AccountsController):
 			"Advance Payment Ledger Entry",
 		)
 		super().on_cancel()
+		self.update_payment_requests(cancel=True)
+		self.update_payment_schedule(cancel=1)
 		self.make_gl_entries(cancel=1)
 		self.update_outstanding_amounts()
 		self.delink_advance_entry_references()
-		self.update_payment_schedule(cancel=1)
-		self.update_payment_requests(cancel=True)
-		self.make_advance_payment_ledger_entries()
-		self.update_advance_paid()  # advance_paid_status depends on the payment request amount
 		self.set_status()
 
 	def update_payment_requests(self, cancel=False):
